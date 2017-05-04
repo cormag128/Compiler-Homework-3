4424bcde9573b8bbb0f2cdd6cb016af2912635c1
Thomas Walters <tcw37@zips.uakron.edu>
2017-05-03 22:05:36 -0400

    Visitor for Decl and Stmt skeleton

--- declare.hpp
+++ declare.hpp
@@ -4,10 +4,22 @@
 struct Type;
 struct Expr;
 struct stmt;
+struct variable_decl;
+struct parameter_decl;
+struct func_decl;
 
 struct decl
 {
+    struct visitor;
     ~decl();
+};
+
+//visitor stuff
+struct decl::visitor
+{
+    virtual void visit(variable_decl*) = 0;
+    virtual void visit(func_decl*) = 0;
+    virtual void visit(parameter_decl*) = 0;
 };
 
 //declare a variable
@@ -16,4 +28,23 @@ struct variable_decl : decl
     symbol* sym;
     Expr* exp;
     Type* typ;
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+//declare a parameter
+struct parameter_decl : decl
+{
+    symbol* sym;
+    Expr* exp;
+    Type* typ;
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+//declare a function
+struct func_decl : decl
+{
+    symbol* sym;
+    Expr* exp;
+    Type* typ;
+    void accept(visitor& v) {return v.visit(this);}
 };
--- statements.hpp
+++ statements.hpp
@@ -1,11 +1,35 @@
 //building statements
 struct Expr;
 struct decl;
+struct Expr_stmt;
+struct decl_stmt;
+struct block_stmt;
+struct ifelse_stmt;
+struct while_stmt;
+struct break_stmt;
+struct continue_stmt;
+struct return_stmt;
+struct assert_stmt;
 
 //statement struct
 struct stmt
 {
+    struct visitor;
     ~stmt() = default;
+};
+
+//visitor stuff
+struct stmt::visitor
+{
+    virtual void visit(Expr_stmt*) = 0;
+    virtual void visit(decl_stmt*) = 0;
+    virtual void visit(block_stmt*) = 0;
+    virtual void visit(ifelse_stmt*) = 0;
+    virtual void visit(while_stmt*) = 0;
+    virtual void visit(break_stmt*) = 0;
+    virtual void visit(continue_stmt*) = 0;
+    virtual void visit(return_stmt*) = 0;
+    virtual void visit(assert_stmt*) = 0;
 };
 
 //Expression statements
@@ -13,6 +37,7 @@ struct Expr_stmt : stmt
 {
     Expr* expression;
     Expr_stmt(Expr* e) : expression(e) {}
+    void accept(visitor& v) {return v.visit(this);}
 };
 
 //Declaration statements
@@ -20,4 +45,40 @@ struct decl_stmt : stmt
 {
     decl* entity;
     decl_stmt(decl* d) : entity(d){}
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+struct block_stmt : stmt
+{
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+struct ifelse_stmt : stmt
+{
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+struct while_stmt: stmt
+{
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+struct break_stmt: stmt
+{
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+struct continue_stmt: stmt
+{
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+struct return_stmt: stmt
+{
+    void accept(visitor& v) {return v.visit(this);}
+};
+
+struct assert_stmt: stmt
+{
+    void accept(visitor& v) {return v.visit(this);}
 };
